f243c57c982f113fbdd05f22a918c33deebf1172
reebass <store1993dron@gmail.com>
2022-10-28 15:19:56 +0300

    one-tast-complete

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+const galletyRefs = document.querySelector('.gallery');
+let showImage ;
+
+const createMarkUp = galleryItems.map(({preview, original, description}) => {
+            return `<div class ="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image" 
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"/>
+        </a>
+        </div>`
+        }).join("");
+
+galletyRefs.innerHTML = createMarkUp;
+galletyRefs.addEventListener('click', modalOpen);
+document.addEventListener('keydown', modalClose)
+
+
+function modalOpen (evt) {
+                evt.preventDefault();
+                if(evt.target.nodeName !== 'IMG') {
+                return
+                }
+                showImage = basicLightbox.create(`<img src="${evt.target.dataset.source}">`)
+                showImage.show()
+}
+
+
+function modalClose (evt) {
+    if (evt.code === 'Escape') {
+    showImage.close()
+}
+}
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galletyRefs = document.querySelector('.gallery');
+// let showImage;
+
+const createMarkUp = galleryItems.map(({preview, original, description}) => {
+            return `<a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+          </a>`
+        }).join("");
+
+galletyRefs.innerHTML = createMarkUp;
+galletyRefs.addEventListener('click', modalOpen);
+document.addEventListener('keydown', modalClose)
+
+                
+function modalOpen (evt) {
+                evt.preventDefault();
+                if(evt.target.nodeName !== 'IMG') {
+                return
+                }
+                
+                new SimpleLightbox(".gallery a", {
+                    captionsData: "alt",
+                    captionDelay: 250,
+                  });
+}
+
+
+function modalClose (evt) {
+    if (evt.code === 'Escape') {
+    showImage.close()
+}
+}
